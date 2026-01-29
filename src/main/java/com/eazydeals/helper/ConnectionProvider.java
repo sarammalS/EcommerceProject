1d6c3fae6bbefc62e210280bbfba37eb0d664ecc
Sarammal S <sarammalbca2003@gmail.com>
2026-01-29 17:03:14 +0530

    Update database connection credentials

--- src/main/java/com/eazydeals/helper/ConnectionProvider.java
+++ src/main/java/com/eazydeals/helper/ConnectionProvider.java
@@ -14,7 +14,7 @@ public class ConnectionProvider extends HttpServlet{
 		try {
 			if (connection == null) {
 				Class.forName("com.mysql.cj.jdbc.Driver");
-				connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/greencartname", "root", "Aqil@786");
+				connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/greencartname", "root", "root");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
